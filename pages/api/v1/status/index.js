d28dd217ad02bbc9567385c64d8780185922d59d
João Marcos Sampaio <joaomarcossandre@hotmail.com>
2025-12-31 00:20:20 -0300

    add: `jsconfig.json`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
-import database from "../../../../infra/database";
+import database from "infra/database";
 
 async function status(request, response) {
   const result = await database.query("SELECT 1+1 as result;");
-  console.log("\x1b[33m", result, "\x1b[0m");
+  console.log(result.rows[0]);
   response.status(200).json({
     status: "ok",
   });
